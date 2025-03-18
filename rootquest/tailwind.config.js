a01cacc116bba85f829f85021e372b8425737c3e
Yussc <yassine.chiadmi3@gmail.com>
2025-03-18 16:11:43 +0100

    zreihfjk

---
+++ rootquest/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // adapte ce chemin en fonction de la structure de ton projet
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
